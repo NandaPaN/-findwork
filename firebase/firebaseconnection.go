351108145d88ea06f1981db5916a1a5423371216
DESKTOP-P7MTEGG\ReN <n>
2023-10-28 01:10:26 +0900

    秘密key

--- firebase/firebaseconnection.go
+++ firebase/firebaseconnection.go
@@ -10,34 +10,35 @@ import (
 	"google.golang.org/api/option"
 )
 
-func main()  {
-	//firebase 接続
+func main() {
+	// Firebase接続
 	ctx := context.Background()
 	sa := option.WithCredentialsFile("serviceAccountKey.json")
 	app, err := firebase.NewApp(ctx, nil, sa)
 
-	if err !=nil {
-		log.fatalf("接続エラーです: %v", err)
+	if err != nil {
+		log.Fatalf("接続エラーです: %v", err)
 	}
 
 	client, firestoreErr := app.Firestore(ctx)
-	if err !=nil {
-		log.fatalf("Firestoreクライアントの初期化エラー: %v \n", firestoreErr)
+	if firestoreErr != nil {
+		log.Fatalf("Firestoreクライアントの初期化エラー: %v \n", firestoreErr)
 	}
 
-	fmt.println(client)
-	fmt.println("接続できた！")
+	fmt.Println("接続できた！")
 
-	//firebase 登録
+	// Firestoreにデータを追加
 	pastTime := time.Date(2023, time.October, 15, 14, 30, 0, 0, time.UTC)
 	data := map[string]interface{}{
-		"Company": "aeon",
+		"Company":       "aeon",
 		"Purchase_date": pastTime,
-		"total": 10000,
+		"total":         10000,
 	}
 
 	_, _, err = client.Collection("レシート情報").Add(ctx, data)
 	if err != nil {
-		log.fatalf("データ追加エラー: %v", err)
+		log.Fatalf("データ追加エラー: %v", err)
 	}
+
+	fmt.Println("データをFirestoreに追加しました！")
 }
